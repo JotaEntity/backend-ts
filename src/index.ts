6f2d2d137241371f36892116feb21c761653a6bf
JotaEntityDev <jotamysteriumdev@gmail.com>
2021-08-26 22:10:42 -0300

    add: test

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,13 @@ const express = require("express");
 // rest of the code remains same
 const app = express();
 const PORT = 8000;
-app.get("/", (req: any, res : any) =>
+app.get("/", (req: any, res: any) =>
   res.send("Programming in TypeScript & deploy in JavaScript")
 );
-app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    app.settings.env,
+    process.env.PORT
+  );
 });
