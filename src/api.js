07fcefff06a2e5cd1ac086034d6080c934073cae
JotaEntityDev <jotamysteriumdev@gmail.com>
2021-08-26 22:35:13 -0300

    rebuild

--- src/api.js
+++ src/api.js
@@ -5,9 +5,7 @@ const app = express();
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.json({
-    hello: "hi!"
-  });
+  res.send('TESTE');
 });
 
 app.use(`/.netlify/functions/api`, router);
