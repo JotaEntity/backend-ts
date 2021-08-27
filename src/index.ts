acc18fc0b5c1db21ef66faa1ff2be7344512b05a
JotaEntityDev <jotamysteriumdev@gmail.com>
2021-08-26 21:06:56 -0300

    add: auto-deploy convert - javascript--ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('default base :D')
+const express = require("express");
+// rest of the code remains same
+const app = express();
+const PORT = 8000;
+app.get("/", (req: any, res : any) =>
+  res.send("Programming in TypeScript & deploy in JavaScript")
+);
+app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+});
